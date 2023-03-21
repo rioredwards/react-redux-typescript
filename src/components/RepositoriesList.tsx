076b5d7354b87a5cfd976d0ea06dba8e0ea7ccb4
Rio Edwards <rioredwards@gmail.com>
2023-03-20 17:40:14 -0700

    Setup custom typed useSelector hook (kind of clunky)

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,14 @@
 import "./RepositoriesList.css";
 import { useState } from "react";
 import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     searchRepositories(term);
